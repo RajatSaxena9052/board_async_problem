e6db85f9c5653f4bbead5f8b13c45f36da0850b3
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-05-30 01:32:12 +0530

    Added callback 1 module

---
+++ callback1.js
@@ -0,0 +1,21 @@
+function callBack1(data, id, callBack) {
+    if (data !== undefined && id !== undefined && callBack !== undefined) {
+
+        setTimeout(() => {
+            let dataGot = data.find((dataElements) => {
+                if (dataElements["id"] == id) {
+                    return dataElements;
+                }
+            });
+
+            if (dataGot === undefined) {
+                callBack("Id Not Found In boards data");
+            } else {
+                callBack(dataGot);
+            }
+
+        }, 2 * 1000);
+
+    }
+}
+module.exports = callBack1;
