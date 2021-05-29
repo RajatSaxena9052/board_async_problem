42d98d3cd5176e24f85c2c90868e5657b9bbd268
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-05-30 02:06:03 +0530

    added space no code change

--- test/testCallback1.js
+++ test/testCallback1.js
@@ -1,9 +1,7 @@
 const boards = require("../data/boards.json");
 const callBack1 = require("../callback1");
-
 let boardId = "mcu453ed";
 
 let callBack = (data) => console.log(data);
-
 
 callBack1(boards, boardId, callBack);
