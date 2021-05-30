ec986836b8401620c2eb44b776197e1324235536
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-05-31 02:12:55 +0530

    Added test module for callBack6 module testCallback6

---
+++ test/testCallback6.js
@@ -0,0 +1,9 @@
+const boards = require("../data/boards.json");
+const lists = require("../data/lists.json");
+const cards = require("../data/cards.json");
+
+const callBack6 = require("../callback6");
+
+let boardId = "mcu453ed";
+
+callBack6(boards, lists, cards, boardId);
